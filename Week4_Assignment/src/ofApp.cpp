daccbcdbc64c4ad4f32becc5a5668458d426dcdc
cair037 <cair037@newschool.edu>
2018-09-21 15:24:49 -0400

    hw4

--- Week4_Assignment/src/ofApp.cpp
+++ Week4_Assignment/src/ofApp.cpp
@@ -2,70 +2,97 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofBackground(255);
+    
+    origin = ofPoint(ofGetWindowWidth()/2,ofGetWindowHeight()/2);
+    origin2= ofPoint(ofGetWindowWidth(),ofGetWindowHeight());
+    origin3= ofPoint(0,ofGetWindowHeight());
+    
+    
+    inputPct = ofGetWindowWidth();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+   
+    inputPct += 0.05;
+    if(inputPct > 5) inputPct = 0;
+    
+   
+    float outputPct = powf(inputPct,3);
+    objectPos = (1-outputPct) * origin + outputPct * dest;
+    objectPos2 =-2*((1-outputPct) * origin + outputPct * dest);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+  
+    
+    ofBackground(180,256,89,5);
+    
+    
+    
+    for(int i = 0; i<15; i++){
+        
+        for (int j = 0; j < 20; j++){
+            if (int j = 10; false);
+            ofPushMatrix();
+            
+            
+            ofTranslate( 30+i * 200, 30+j * 50);
+            
+            
+            
+            
+            
+            
+            float y = y/2+ ofRandom(-20,20);
+            
+            ofFill();
+            ofSetColor( 232, 247, 144, 30);
+            ofDrawCircle(y/50, x*30*(x-30), 15);
+            
+            
+            
+            ofFill();
+            ofSetColor( 232, 247, 144, 60);
+            ofDrawCircle(y/50, x*30*(x-20), ofGetWindowHeight()/(5*inputPct-y/2)/30);
+            
+            
+            
+            
+            
+            float x = x/2+ ofRandom(-20,20);
+            
+            
+            
+            ofFill();
+            ofSetColor( 255, 255, 255, 30);
+            ofDrawCircle(x*3*(x-80), y/5, ofGetWindowHeight()/inputPct/3/50);
+            
+           
+        
+            
+            
+            
+            ofPopMatrix();
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+ 
+    
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
 
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
--- Week4_Assignment/src/ofApp.h
+++ Week4_Assignment/src/ofApp.h
@@ -3,22 +3,26 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    
+    ofPoint origin;
+    ofPoint dest;
+    ofPoint objectPos;
+    ofPoint objectPos2;
+    ofPoint origin2;
+    ofPoint origin3;
+    
+    //the x axis of math graph
+    float inputPct;
+    float outputPct;
+    float x;
+    float y;
+    float mouseMove;
 };
+
+
